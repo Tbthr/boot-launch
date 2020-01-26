6cf9d67476f5a949b2c644732aebc98485264d93
Tbthr <1598622578@qq.com>
2020-01-26 16:16:24 +0800

    4-2 Spring JDBC多数据源的实现

---
+++ src/main/java/com/power/bootlaunch/config/DataSourceConfig.java
@@ -0,0 +1,41 @@
+package com.power.bootlaunch.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Primary
+    @Bean(name = "primaryDataSource") //为该Bean命名，并注入
+    @Qualifier("primaryDataSource") // 根据Bean的名称寻找
+    @ConfigurationProperties(prefix="spring.datasource.primary") //
+    public DataSource primaryDataSource() {
+            return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate (
+        @Qualifier("primaryDataSource") DataSource dataSource ) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name="secondaryJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
--- src/main/java/com/power/bootlaunch/dao/ArticleJDBCDAO.java
+++ src/main/java/com/power/bootlaunch/dao/ArticleJDBCDAO.java
@@ -12,48 +12,60 @@ import java.util.List;
 public class ArticleJDBCDAO {
 
     @Resource
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate primaryJdbcTemplate;
+
+    @Resource
+    private JdbcTemplate secondaryJdbcTemplate;
 
     //保存文章
     public void save(Article article) {
         //jdbcTemplate.update适合于insert 、update和delete操作；
-        jdbcTemplate.update("INSERT INTO article(author, title,content,create_time) values(?, ?, ?, ?)",
+        primaryJdbcTemplate.update("INSERT INTO article(author, title,content,create_time) values(?, ?, ?, ?)",
                 article.getAuthor(),
                 article.getTitle(),
                 article.getContent(),
                 article.getCreateTime());
-
+        secondaryJdbcTemplate.update("INSERT INTO article(author, title,content,create_time) values(?, ?, ?, ?)",
+                article.getAuthor(),
+                article.getTitle(),
+                article.getContent(),
+                article.getCreateTime());
     }
 
     //删除文章
     public void deleteById(Long id) {
         //jdbcTemplate.update适合于insert 、update和delete操作；
-        jdbcTemplate.update("DELETE FROM article WHERE id = ?", new Object[]{id});
-
+        primaryJdbcTemplate.update("DELETE FROM article WHERE id = ?", new Object[]{id});
+        secondaryJdbcTemplate.update("DELETE FROM article WHERE id = ?", new Object[]{id});
     }
 
     //更新文章
     public void updateById(Article article, Long id) {
         //jdbcTemplate.update适合于insert 、update和delete操作；
-        jdbcTemplate.update("UPDATE article SET author = ?, title = ? ,content = ?,create_time = ? WHERE id = ?",
+        primaryJdbcTemplate.update("UPDATE article SET author = ?, title = ? ,content = ?,create_time = ? WHERE id = ?",
                 article.getAuthor(),
                 article.getTitle(),
                 article.getContent(),
                 article.getCreateTime(),
                 id);
-
+        secondaryJdbcTemplate.update("UPDATE article SET author = ?, title = ? ,content = ?,create_time = ? WHERE id = ?",
+                article.getAuthor(),
+                article.getTitle(),
+                article.getContent(),
+                article.getCreateTime(),
+                id);
     }
 
     //根据id查找文章
     public Article findById(Long id) {
         //queryForObject用于查询单条记录返回结果
-        return (Article) jdbcTemplate.queryForObject("SELECT * FROM article WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper(Article.class));
+        return  (Article) primaryJdbcTemplate.queryForObject("SELECT * FROM article WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper(Article.class));
     }
 
     //查询所有
     public List<Article> findAll() {
         //query用于查询结果列表
-        return (List<Article>) jdbcTemplate.query("SELECT * FROM article ", new BeanPropertyRowMapper(Article.class));
+        return (List<Article>) primaryJdbcTemplate.query("SELECT * FROM article ", new BeanPropertyRowMapper(Article.class));
     }
 
 }
